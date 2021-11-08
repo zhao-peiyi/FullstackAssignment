d9ff77c3c1569712a2305588e4a2a184423397ba
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-08 21:19:13 +0800

    commit for 1.5

--- part 1/courseinfo/src/App.js
+++ part 1/courseinfo/src/App.js
@@ -23,8 +23,9 @@ const Total = ( props ) => (
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10
@@ -37,13 +38,14 @@ const App = () => {
         name: 'State of a component',
         exercises: 14
       }
-  ]
+    ]
+  }
 
   return (
     <div>
-      <Header course={ course } />
-      <Content parts={ parts } />
-      <Total parts={ parts } />
+      <Header course={ course.name } />
+      <Content parts={ course.parts } />
+      <Total parts={ course.parts } />
     </div>
   )
 }
