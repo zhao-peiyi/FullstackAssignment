55de74188d5ef24d2d0c176c7302f595e97d959b
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-13 14:07:19 +0800

    commit for 2.2

--- part 2/Course information/src/index.js
+++ part 2/Course information/src/index.js
@@ -25,9 +25,7 @@ const Part = (props) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      { course.parts.map ( (part) => <Part key={part.id} part={part} /> )}
     </div>
   )
 }
