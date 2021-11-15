d91e412017a8d8e9114a475aafa816c99e4c868c
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-15 11:55:50 +0800

    commit for 2.6

---
+++ part 2/The Phonebook/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPersons = persons.concat({name:newName})
+    // console.log(newPersons)
+    setPersons(newPersons)
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      { persons.map( person => <div key={person.name}>{person.name}</div> )}
+    </div>
+  )
+}
+
+export default App
---
+++ part 2/The Phonebook/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App notes={[{id:1,content:1},{id:2,content:2},{id:3,content:3}]}/>, document.getElementById('root'))
