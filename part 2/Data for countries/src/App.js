2312ced7e96a7a72e711be4204b96ee9edb19417
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-18 23:35:04 +0800

    commit for 2.12

---
+++ part 2/Data for countries/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState , useEffect } from 'react'
+import axios from 'axios'
+
+const Detail = ( {country} ) => {
+  // console.log(country)
+  return (
+    <div>
+      <h1>{ country.name.common }</h1>
+      <p>capital { country.capital }</p>
+      <p>area { country.area }</p>
+      <h2>languages</h2>
+      <ul>
+        { Object.values(country.languages).map( language => <li key={language}>{language}</li>)}
+      </ul>
+      <img src={country.flags.png} alt={country.name.common}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const [ text, setText ] = useState('')
+  const [ countries, setCountries ] = useState([])
+  const [ result, setResult ] = useState('')
+
+  useEffect(()=>{
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then( response => {
+        setCountries( response.data)
+      })
+  },[])
+  // console.log( countries )                         // test for first and second rendering
+
+  const handleInputText = ( event ) => {
+    // console.log( event.target.value )
+    setText( event.target.value )                  // It could also work without variable text, just use event.target.value
+    filter(event.target.value )
+  }
+
+  const filter = ( oringialText ) => {
+    const text = oringialText.toLowerCase()
+    if ( text === '' ) {
+      setResult('')
+    } else {
+      const list = countries.filter( country => {
+        const countryName = country.name.common.toLowerCase()
+        return countryName.indexOf( text ) !== -1
+      })
+      if ( list.length > 10 ) {
+        setResult('Too many matches, specify another filter')
+      } else if ( list.length > 1 ) {
+        setResult( list.map( country => <p key={country.name.common}>{country.name.common}</p>))
+      } else if (list.length === 1 ) {
+        setResult(<Detail country= { list[0] } />)
+      } else {
+        setResult('')
+      }
+    }
+  }
+
+
+  return (
+    <div>
+      <div>find countries <input value={text} onChange={handleInputText}/></div>
+      <div>{result}</div>
+    </div>
+  )
+}
+
+export default App
