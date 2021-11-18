8625cdcc9485bcb7606934ddfb1c1d6d88967a75
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-18 22:02:32 +0800

    commit for 2.11

--- part 2/The Phonebook/src/App.js
+++ part 2/The Phonebook/src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterToShow, setFilterToShow ] = useState( persons )
+
+  useEffect (()=>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then( response => {
+        // console.log(response.data)
+        setPersons( response.data )
+        setFilterToShow( response.data )
+      })
+  },[])
 
   const addName = (event) => {
     event.preventDefault()
