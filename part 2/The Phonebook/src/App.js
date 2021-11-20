34aab61a906a14d88db95edbcecf8c149d421ed4
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-20 12:04:37 +0800

    commit for 2.15

--- part 2/The Phonebook/src/App.js
+++ part 2/The Phonebook/src/App.js
@@ -29,15 +29,22 @@ const App = () => {
     if ( persons.some(checkExistedName) ) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      const newPersons = persons.concat({
+      const newPerson = {
         name: newName,
         number: newNumber,
         id: persons.length + 1,
-      })
-      // console.log(newPersons)
-      setPersons(newPersons)
-      setNewName('')
-      setNewNumber('')
+      }
+
+      axios
+        .post( 'http://localhost:3001/persons', newPerson )           // send information to server
+        .then( response => {
+          const newPersons = persons.concat( response.data )          // show information on browser
+          setPersons(newPersons)
+          setFilterToShow(newPersons)
+
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
