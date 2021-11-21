8b5ea662fd8c5e2dca17f56ddfb2deff3c3743b9
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-21 23:28:30 +0800

    commit for 3.4

--- part 3/PhonebookBackend/index.js
+++ part 3/PhonebookBackend/index.js
@@ -1,7 +1,7 @@
 const express = require( 'express' )
 const app = express()
 
-const data = [
+let data = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -49,6 +49,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id' , (request, response) => {
+  const id = Number( request.params.id )
+  data = data.filter( person => person.id !== id )
+
+  response.status(204).end()
 })
 
 const PORT = 3001
