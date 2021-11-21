505b5fc942d0e74b118d7fe3d3d0bced23b09645
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-21 22:48:19 +0800

    commit for 3.3

--- part 3/PhonebookBackend/index.js
+++ part 3/PhonebookBackend/index.js
@@ -40,6 +40,17 @@ app.get('/info', (request, response) => {
   response.send(info)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number( request.params.id )
+  const person = data.find( person => person.id === id )
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen( PORT, () => {
   console.log( `Server running on port ${PORT}` );
