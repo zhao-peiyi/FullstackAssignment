88332a1cf2bff4a04bf0455959c64079b6911e8d
zhao-peiyi <nora.zhaopeiyi@gmail.com>
2021-11-22 22:10:08 +0800

    commit for 3.8

--- part 3/PhonebookBackend/index.js
+++ part 3/PhonebookBackend/index.js
@@ -5,7 +5,9 @@ const app = express()
 
 const jsonParser = express.json()
 app.use( jsonParser )
-app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let data = [
     {
